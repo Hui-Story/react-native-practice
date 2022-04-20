1042ce2150c452627c2179b06954b988b06925a7
Hui-Story <leejaehee0728@gmail.com>
2022-04-20 22:59:40 +0900

    ENG: update LearnReactNative project
    LearnReactNative 프로젝트 코드 작성
    2장 컴포넌트.md 파일 작성

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Button} from 'react-native';
+import Greeting from './components/Greeting';
+import Box from './components/Box';
+import Counter from './components/Counter';
 
 const App = () => {
+  const [visible, setVisible] = useState(true);
+  const [count, setCount] = useState(0);
+
+  const onPress = () => {
+    setVisible(!visible);
+  };
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello React!</Text>
-      </View>
+    <SafeAreaView style={styles.full}>
+      <Greeting />
+      <Button title="토글" onPress={onPress} />
+      {visible && <Box rounded={true} size="large" color="blue" />}
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 export default App;
